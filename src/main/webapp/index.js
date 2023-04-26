67e7b40954661f8780d34138c5b3e1039122d611
ArinNene <arnene@usc.edu>
2023-04-25 18:56:38 -0700

    added index page

---
+++ src/main/webapp/index.js
@@ -0,0 +1,27 @@
+// type LatLngLiteral = google.maps.LatLngLiteral;
+// type MapOptions = google.maps.MapOptions;
+
+const loader = new loader({
+	apiKey: "AIzaSyCpCVaKxl81vxeIZ7_-bpF7Zj_2x1vZ34s",
+	version: "weekly",
+});
+
+const options = useMemo(
+	({
+		disableDefaultUI : true,
+		clickableIcons : false,
+		mapId: "f3947d79eba2c6f",	
+	})
+);
+
+loader.load().then(async() => {
+	const { Map } = await google.maps.importLibrary("maps");
+	
+	map = new Map(document.getElementById("map"), {
+		center : {lat: 34.02127561489714, lng: -118.2866354425022},
+		zoom : 15,
+		mapContainerClassName : "map-container",
+		options : {options}
+	});
+	
+});
