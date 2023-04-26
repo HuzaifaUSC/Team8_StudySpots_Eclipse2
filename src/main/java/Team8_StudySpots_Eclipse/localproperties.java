e186606a3c093dbded271f5022dd54466075b708
ArinNene <arnene@usc.edu>
2023-04-25 19:24:59 -0700

    Added index pages

--- src/main/java/Team8_StudySpots_Eclipse/localproperties.java
+++
@@ -1,7 +0,0 @@
-package Team8_StudySpots_Eclipse;
-
-public class localproperties {
-	public static String MYSQL_JDBC_LINK = "jdbc:mysql://localhost/studyspots?user=root&password=cx3M1-lMp2R-JdnxK";
-	
-	
-}
--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,10 +1,6 @@
 // type LatLngLiteral = google.maps.LatLngLiteral;
 // type MapOptions = google.maps.MapOptions;
 
-const loader = new loader({
-	apiKey: "AIzaSyCpCVaKxl81vxeIZ7_-bpF7Zj_2x1vZ34s",
-	version: "weekly",
-});
 
 const options = useMemo(
 	({
@@ -14,10 +10,15 @@ const options = useMemo(
 	})
 );
 
+const loader = new loader({
+	apiKey: "AIzaSyCpCVaKxl81vxeIZ7_-bpF7Zj_2x1vZ34s",
+	version: "weekly",
+});
+
 loader.load().then(async() => {
 	const { Map } = await google.maps.importLibrary("maps");
 	
-	map = new Map(document.getElementById("map"), {
+	map = new google.maps.Map(document.getElementById("map"), {
 		center : {lat: 34.02127561489714, lng: -118.2866354425022},
 		zoom : 15,
 		mapContainerClassName : "map-container",
